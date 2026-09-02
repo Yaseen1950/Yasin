5d7ca098f6d8241e84fa82e707d31434e24d7382
Yaseen1950 <yasin1950gmail@gmail.com>
2026-09-02 10:17:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import os
 import telebot
 
-TOKEN = "8428439895:AAH11iI7_G4K4J4jlvWH-Jx4-RtvSBMk_VI"
+TOKEN = "8428439895:AAH11iI7_G4K4J4jlvWH-Jx4-RtvSBmK_VI"
 bot = telebot.TeleBot(TOKEN)
 
-
 @bot.message_handler(commands=["start"])
-def send_welcome(message):
-  bot.reply_to(message, "أهلاً بك، أنا بوت ياسين أعمل بنجاح على منصة Railway!")
-
+ def send_welcome(message):
+    bot.reply_to(message, "أهلاً بك يا ياسين، Railway")
+ 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
