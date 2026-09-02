b25f9594f12d245d2ba0a5dd29827f039946ae59
Yaseen1950 <yasin1950gmail@gmail.com>
2026-09-02 10:01:04 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+
+TOKEN = "8428439895:AAH11iI7_G4K4J4jlvWH-Jx4-RtvSBMk_VI"
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "أهلاً بك، أنا بوت ياسين أعمل بنجاح على منصة Railway!")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+  bot.reply_to(message, f"أهلاً بك! لقد استقبلت رسالتك: {message.text}")
+
+
+bot.infinity_polling()
